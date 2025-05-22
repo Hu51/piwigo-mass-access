3be7ff284089b1aebfd09d2c3672efd5ab6bb198
Gergely Szabo <greg@hu51.hu>
2025-05-22 16:20:19 +0200

    add admin menu

--- main.inc.php
+++ main.inc.php
@@ -15,6 +15,7 @@ define('MASS_ACCESS_ADMIN', get_root_url() . 'admin.php?page=plugin-' . MASS_ACC
 
 // Add the plugin to the admin menu
 add_event_handler('loc_end_section_init', 'mass_access_init');
+add_event_handler('loc_end_section_init', 'mass_access_admin_menu');
 
 function mass_access_init()
 {
@@ -31,5 +32,33 @@ function mass_access_init()
         
         $template->set_filename('mass_access_admin', realpath(MASS_ACCESS_PATH . 'template/admin.tpl'));
         $template->assign('MASS_ACCESS_CONTENT', $template->parse('mass_access_admin', true));
+    }
+}
+
+function mass_access_admin_menu()
+{
+    global $template, $user;
+    
+    if (script_basename() == 'admin' && $user['is_admin'])
+    {
+        $template->assign(
+            array(
+                'MASS_ACCESS_ADMIN' => MASS_ACCESS_ADMIN,
+            )
+        );
+        
+        $template->assign(
+            'PLUGIN_INDEX_ACTIONS',
+            array_merge(
+                $template->get_template_vars('PLUGIN_INDEX_ACTIONS'),
+                array(
+                    array(
+                        'URL' => MASS_ACCESS_ADMIN,
+                        'TITLE' => 'Mass Access Management',
+                        'ICON' => 'fa-users'
+                    )
+                )
+            )
+        );
     }
 } 
